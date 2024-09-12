fd044ef54eb421cb708c414d93fdf5b33c6e5391
Daniel Garcia Rojas <d.g.r.developer@gmail.com>
2024-09-12 05:28:44 -0300

    amazon cogito implementacion

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-1_diwAED1nX', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '36f8323rgtd5m6fl9gno5gppc2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-1' // e.g. us-east-2
+        userPoolId: 'us-east-2_ja75VSjPH', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '	1n0af4gdmivcuc84uu1eqtshbg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-est-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://741g024jac.execute-api.us-west-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
