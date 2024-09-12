1d0408bd1a68f1883d7d3d7f405c713a84c47838
Daniel Garcia Rojas <d.g.r.developer@gmail.com>
2024-09-12 17:42:42 -0300

    new_configuration

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-2_ja75VSjPH', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '	1n0af4gdmivcuc84uu1eqtshbg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '1n0af4gdmivcuc84uu1eqtshbg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://enkudtjyd3.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
 
