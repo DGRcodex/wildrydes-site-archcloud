e907bfbffad544948167b880f1084b91117d445e
Daniel Garcia Rojas <d.g.r.developer@gmail.com>
2024-09-12 06:12:51 -0300

    correción

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-2_ja75VSjPH', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '	1n0af4gdmivcuc84uu1eqtshbg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-est-2' // e.g. us-east-2
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
